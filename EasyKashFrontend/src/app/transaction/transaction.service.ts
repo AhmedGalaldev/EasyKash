a6084c8e115be69a0cb87c884942a902ff423146
ahmedgalaldev <ahmedgalaldev@gmail.com>
2020-11-18 19:17:12 +0200

    last commit

--- EasyKashFrontend/src/app/transaction/transaction.service.ts
+++ EasyKashFrontend/src/app/transaction/transaction.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { map, catchError, tap } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 import { Transaction } from './transaction.model';
 
 @Injectable({
