45d308adf1dabcd077b83027d9839ab21cedf42e
ahmedgalaldev <ahmedgalaldev@gmail.com>
2020-11-18 11:52:11 +0200

    setup bootstrap and create transaction component

--- EasyKashFrontend/src/app/app.module.ts
+++ EasyKashFrontend/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TransactionComponent } from './transaction/transaction.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TransactionComponent
   ],
   imports: [
     BrowserModule
